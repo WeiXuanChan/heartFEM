e002d2a49b9adff683eac30bf1493c5e450bc118
W. X. Chan <w.x.chan1986@gmail.com>
2021-03-08 17:45:08 +0800

    Update __init__.py

--- heartFEM/ngspice_py/__init__.py
+++ heartFEM/ngspice_py/__init__.py
@@ -1,8 +1,18 @@
-import os as os
-import sys
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+'''
+File: __init__.py
+Description: load all class for ngspice_py
+             Contains externally usable class
+History:
+    Date    Programmer SAR# - Description
+    ---------- ---------- ----------------------------
+  Author: w.x.chan@gmail.com         08MAR2021           - Created
+  Author: w.x.chan@gmail.com         08MAR2021           - v1.0.0
+                                                            -createLVcircuit v1.0.0
+                                                            -simLVcircuit v1.0.0
+                                                            -generateLVtable v1.0.0
+'''
+_version='1.0.0'
 
-from createLVcircuit         import *
-from simLVcircuit            import *
-from generateLVtable         import *
-
+from heartFEM.ngspice_py.createLVcircuit         import *
+from heartFEM.ngspice_py.simLVcircuit            import *
+from heartFEM.ngspice_py.generateLVtable         import *
