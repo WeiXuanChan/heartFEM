b885484f90fa0350c65ba44a44c0781d7ce3a8bf
W. X. Chan <w.x.chan1986@gmail.com>
2021-04-21 16:43:13 +0800

    Update __init__.py

--- heartFEM/ngspice_py/__init__.py
+++ heartFEM/ngspice_py/__init__.py
@@ -15,8 +15,13 @@ History:
                                                             -simLVcircuit v2.0.0
                                                             -generateLVtable v2.0.0
                                                             -simLVcircuit_alignEStime v2.0.0
+  Author: w.x.chan@gmail.com         21APR2021           - v2.1.0
+                                                            -createLVcircuit v2.1.0
+                                                            -simLVcircuit v2.1.0
+                                                            -generateLVtable v2.1.0
+                                                            -simLVcircuit_alignEStime v2.1.0
 '''
-_version='2.0.0'
+_version='2.1.0'
 
 from heartFEM.ngspice_py.createLVcircuit            import *
 from heartFEM.ngspice_py.simLVcircuit               import *
