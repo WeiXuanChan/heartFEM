e911d3d848199b624e128dd811485596d4931e46
W. X. Chan <w.x.chan1986@gmail.com>
2021-10-28 12:09:31 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ setup(
         'ngspice':['*'],
         'lcleeHeart':['*','vtk_py/*']
         },
-  version = '3.6.1',      # Start with a small number and increase it with every change you make
+  version = '3.6.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Optimization of patient specific heart model with myocardium FEM and cardiovascular network Windkessel.',   # Give a short description about your library
   author = 'Wei Xuan Chan',                   # Type in your name
   author_email = 'w.x.chan1986@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/WeiXuanChan/heartFEM',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/WeiXuanChan/heartFEM/archive/v3.6.1.tar.gz',    
+  download_url = 'https://github.com/WeiXuanChan/heartFEM/archive/v3.6.2.tar.gz',    
   keywords = ['medical', 'cardiac'],   # Keywords that define your package best
   install_requires=['numpy','matplotlib','scipy'],
   extras_require = {
